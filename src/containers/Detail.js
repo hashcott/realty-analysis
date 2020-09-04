7ad240601d5651865c0128d36f2d088818eb1ce9
binh <nguyenducbinh8920@gmail.com>
2020-09-04 18:52:27 +0700

    fix detail

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -1,28 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import SpinnerButton from 'react-native-spinner-button';
 export const Detail = () => {
   return (
     <View style={styles.container}>
       <ScrollView>
       <View style={styles.info}>
-        <View style={styles.picture}></View>
+        <Image style={styles.picture}/>
         <Text style={styles.name}>Name of house</Text>
         <Text style={styles.location}>Location </Text>
       </View>
-      <View style={styles.follow}>
-        <Text style={{color: 'white',paddingTop:5}}>Follow</Text>
+      <View>
+      <SpinnerButton buttonStyle={styles.buttonFollow} >
+        <Text style={{color: 'white'}}>Follow</Text>
+      </SpinnerButton>
       </View>
-      <View style={styles.call}>
-        <Text style={{color: 'black', paddingTop:5}}>Call</Text>
+      <View>
+      <SpinnerButton buttonStyle={styles.buttonCall} >
+        <Text style={{color: 'black'}}>Call</Text>
+      </SpinnerButton>
       </View>
       <View style={styles.architecture}>
-        <View style={styles.picture1}></View>
-        <View style={styles.picture2}></View>
+        <Image style={styles.picture1}/>
+        <View style={styles.info2}></View>
       </View>
-      <View style={styles.more}>
-        <Text style={{color: 'black', paddingTop:5}}>See More</Text>
+      <View>
+        <SpinnerButton buttonStyle={styles.buttonCall} >
+          <Text style={{color: 'black'}}>See More</Text>
+        </SpinnerButton>
       </View>
       </ScrollView>
     </View>
@@ -31,7 +37,7 @@ export const Detail = () => {
 
 const styles = StyleSheet.create({
   info: {
-    marginTop: 30,
+    marginTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
@@ -49,27 +55,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 14,
   },
-  follow: {
-    marginLeft: 40,
-    marginBottom: 20,
-    alignItems: 'center',
-    width: 340,
-    height: 60,
-    padding: 12,
-    backgroundColor: 'black',
-    borderRadius: 10,
-  },
-  call: {
-    marginLeft: 40,
-    marginBottom: 20,
-    alignItems: 'center',
-    width: 340,
-    height: 60,
-    padding: 12,
-    borderColor:'black',
-    borderRadius: 10,
-    borderWidth: 2,
-  },
   architecture: {
     alignItems:'center',
     justifyContent: 'center'
@@ -80,21 +65,30 @@ const styles = StyleSheet.create({
     backgroundColor: 'burlywood',
     marginBottom: 30,
   },
-  picture2: {
+  info2: {
     width: 300,
     height: 350,
     backgroundColor: 'deepskyblue',
     marginBottom: 50,
   },
-  more: {
-    marginLeft: 40,
-    marginBottom: 20,
+  buttonFollow: {
+    margin:32,
+    justifyContent: 'center',
     alignItems: 'center',
-    width: 340,
     height: 60,
-    padding: 12,
-    borderColor:'black',
     borderRadius: 10,
-    borderWidth: 2,
-  }
+    backgroundColor: 'black'
+  },
+  buttonCall: {
+    marginBottom: 30,
+    marginLeft: 32,
+    marginRight: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 2
+  },
 });
