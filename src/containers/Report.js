345c45d0b5b51a768432b3a6e404d854375ee3c4
Trang Hoang <tranghoang200@gmail.com>
2020-09-05 16:06:46 +0700

    Update Report.js

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createExample } from "../actions/Example";
 import { connect } from "react-redux";
+import { DATA } from "../DummyData";
+import ItemReport from "../components/ItemReport";
 
-const Report = () => {
+
+const Report = ({navigation}) => {
+  const HandleClick = (item) => {
+    let type= item.x;
+    let data= item.reportData;
+    let predictText = item.predictText;
+    navigation.navigate('DetailReport', {
+      type,
+      data,
+      predictText
+    });
+  }
+  const renderItem = ({ item, navigation }) => <ItemReport item={item} HandleClick={HandleClick} />;
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Report</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Text style={{ margin: 15, fontSize: 18, fontWeight: "500" }}>
+        Tháng 1 - Tháng 6
+      </Text>
+      <View style={styles.container}>
+        <FlatList data={DATA} renderItem={renderItem}/>
+      </View>
     </View>
   );
 };
@@ -17,4 +37,8 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, { createExample })(Report);
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {  
+    flex: 1,
+  },
+});
